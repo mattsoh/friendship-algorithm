35a6f00185e0fd545649bdc89df43da393b9971b
Matthew <mattsoh100@gmail.com>
2024-07-31 00:25:42 +0800

    finished!

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# This Python code is inspired from The Friendship Algorithm mentioned in The Big Bang Theory in Season 2, Episode 13.
+# It is slightly modified to account for inifinite loops and invalid user inputs.
+# Note that the ESC where recreational activities are compared is set to n > 6.
+# Created by Matthew Soh
+
 RED = "\033[0;31m"
 RESET = "\033[0m"
 
@@ -14,6 +19,8 @@ def invInp(i):
   if i == 3:
       print(RED + "\nSystem Termination.")
       quit()
+
+print(RED + "Note: The user will have to act as both Sheldon Cooper and Barry Kripke. However, this code imitates the watcher's view of only hearing what Cooper says.")
 
 print()
 print(RESET + "[Placing Phone Call...]")
@@ -49,4 +56,59 @@ for i in range(4):
   else:
     invInp(i)
 
+print()
+cooper("Do you enjoy a hot beverage?")
+for i in range(4):
+  print()
+  system("What beverage does Kripke enjoy?")
+  bevAns = input("Input ('Tea', 'Coffee', 'Cocoa' is preferred): ")
+  if bevAns == "Tea" or bevAns == "Coffee" or bevAns == "Cocoa":
+    bevAns.lower()
+    print("[Cooper and Kripke share %s together.]" %bevAns)
+    quit()
+  else:
+    break
 
+recs = []
+i = 0
+n = 0
+chooseAct = False
+while True:
+  print()
+  cooper("Recreational activity? Tell me one of your interests!")
+  system("What recreational activity does Kripke want to offer?")
+  recAns = input("Input (Recreational Activity): ")
+  system("Does Cooper like this activity?")
+  likeAns = inpMess()
+  if likeAns == "Yes":
+    break
+  elif likeAns == "No":
+    system("Oh well...")
+    recs.append(recAns)
+    if n == 5:
+      chooseAct = True
+      break
+    system("Let's try again!")
+    n += 1
+  else:
+    i += 1
+    invInp(i)
+  
+print()
+if chooseAct:
+  system("Cooper will have to choose the least objectionable action.")
+  for i,c in enumerate(recs):
+    print("Option %d: "%(i+1), c)
+  system("Which option does Cooper choose?")
+  for i in range(4):
+    print()
+    try: 
+      optionAns = int(input("Input (no. from 1 onwards): "))
+      cooper(recs[optionAns-1]+'!')
+      break
+    except:
+      invInp(i)
+cooper("Why don't we do that together?")
+
+
+  
